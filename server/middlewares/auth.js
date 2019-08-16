a710645ac697f7056a4f201bb0619a36e7d4cc98
mudrakpatel <mudrakpatel96@gmail.com>
2019-08-16 14:43:48 -0400

    Added authentication

---
+++ server/middlewares/auth.js
@@ -0,0 +1,20 @@
+const jsonWebToken = require("jsonwebtoken");
+
+module.exports = (request, response, next) => {
+  if(request.headers.authorization){
+    const token = request.headers.authorization.split(" ")[1];
+    jsonWebToken.verify(token,
+      process.env.SECRET,
+      (error, decoded) => {
+        if(error){
+          next(Error("Failed to authenticate token"));
+        }else{
+          request.decoded = decoded;
+          next();
+        }
+      });
+
+  }else{
+    next(Error("No token provided"));
+  }
+}
